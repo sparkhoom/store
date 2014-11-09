e1e4cd1519e37b533742e469ff3b13ac650bdb89
sparkhoom <176072396@qq.com>
2014-11-09 20:21:41 +0800

    add session controller spec

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -4,16 +4,49 @@ RSpec.describe Api::V1::SessionsController, :type => :controller do
 
 		before(:all) do
 		end
-	    it "should be json" do
+	    it "login in successully" do
 	      user = create(:user)
 		  @request.env["devise.mapping"] = Devise.mappings[:user]
 	      # user = User.new(:name => 'testuser', :email => 'user@example.com', :password => 'secret', :password_confirmation => 'secret')
 	      post :create, user: {email: user.email, password: user.password}
+	      body = JSON.parse(response.body)
+
+	      expect(body["success"]).to eq true
 		  expect(response).to have_http_status(200)
 	    end
 
-	    it "renders the index template" do
-	      
+	    it "login in unsucessfully" do
+	      user = create(:user)
+		  @request.env["devise.mapping"] = Devise.mappings[:user]
+		  post :create, user: {email: user.email, password: "invaid password"}
+		  expect(response).to have_http_status(401)
 	    end
+
+	    it "log out sucessfuly" do
+	      user = create(:user)
+		  @request.env["devise.mapping"] = Devise.mappings[:user]
+		  post :create, user: {email: user.email, password: user.password}
+		  auth_params = {
+		        "auth_token" => user.authentication_token
+		   }
+		  request_headers = {
+	        "Accept" => "application/json",
+	        "Content-Type" => "application/json"
+	      }
+		  delete :destroy, auth_params,request_headers
+      	  expect(response.status).to eq 200
+	    end
+
+	    it "log out notsucessfuly" do
+	      user = create(:user)
+		  request.env["devise.mapping"] = Devise.mappings[:user]
+		  post :create, user: {email: user.email, password: user.password}
+		  delete :destroy
+		  expect(response.status).to eq 404
+	    end
+
+	    def response_body
+    		JSON.parse(response.body)
+  		end
 
 end
