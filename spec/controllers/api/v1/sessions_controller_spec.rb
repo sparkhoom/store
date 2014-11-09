6243fec15b06ea6386b95b884c0c7227217d757e
sparkhoom <176072396@qq.com>
2014-11-09 12:58:20 +0800

    sucessful add spec sessionscontroller

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
-
+require 'devise'
 RSpec.describe Api::V1::SessionsController, :type => :controller do
 
 	    it "should be json" do
 	      @request.env["devise.mapping"] = Devise.mappings[:user]
 	      p "hello world"
-	      # post :create, "session"=>{"user"=>{"email"=>"user@example.com", "password"=>"11111111"}}
+	      p warden
+	      # user = User.new(:name => 'testuser', :email => 'user@example.com', :password => 'secret', :password_confirmation => 'secret')
+	      post :create, "user" => {"email" =>"user@example.com", "password" => "123123"}
 	   #    p @request.params
 	   #    # , :user => {:email =>"user@example.com", :password => "123123", :password_confirmation =>"123123"}
-		  # expect(page).to have_http_status(200)
+		  expect(response).to have_http_status(200)
 	    end
 
 	    it "renders the index template" do
